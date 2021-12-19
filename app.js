51d58bf86b671ec432afe4d023f57ab4d370458a
Jess <92649040+jhessicafaus@users.noreply.github.com>
2021-12-19 18:34:50 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const timeLeft = document.querySelector('#time-left') // .getElementById can also be used, but with .querySelector it's possible to use CSS selectors
+const timeLeft = document.querySelector('#time-left') 
+// .getElementById can also be used above, but with .querySelector it's possible to use CSS selectors
 const scoreDisplay = document.querySelector('#score')
 const squares = document.querySelectorAll('.square')
 
