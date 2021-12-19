4ba625405168fad049b93ea1a1e86307b67dfd59
jhessicafaus <92649040+jhessicafaus@users.noreply.github.com>
2021-12-19 14:02:25 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,42 @@
+const timeLeft = document.querySelector('#time-left') // .getElementById can also be used here instead of .querySelector
+const scoreDisplay = document.querySelector('#score')
+const squares = document.querySelectorAll('.square')
+
+let molePosition
+let score = 0
+let currentTime = 60
+let countDownTimerId
+let moleTimerId
+
+function moveMole() {
+    moleTimerId = setInterval(function() {
+        squares.forEach(square => square.classList.remove('mole'))
+        const randomSquare = squares[Math.floor(Math.random() * squares.length)]
+        randomSquare.classList.add('mole')
+
+        molePosition = randomSquare.id
+
+    } ,500)
+}
+
+moveMole()
+
+squares.forEach(square => square.addEventListener('click', function(){
+    if (square.id === molePosition) {
+        score++
+        scoreDisplay.innerHTML = score
+    }
+
+}))
+
+function countDown() {
+    currentTime--
+    timeLeft.innerHTML = currentTime
+
+    if (currentTime === 0) {
+        clearInterval(countDownTimerId)
+        clearInterval(moleTimerId)
+    }
+}
+
+countDownTimerId = setInterval(countDown, 1000)
